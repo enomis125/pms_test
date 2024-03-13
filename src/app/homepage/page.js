ed69cc12757fb58035a5917d7c86162ccafce03f
Bruno Moreira <kayunowna@gmail.com>
2024-03-13 22:05:24 +0000

    Demo API

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -1,13 +1,33 @@
 "use client"
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Homepage = (pagina) => {
 
+    const [caracteristics, setCaracteristics] = useState([]);
+
+    useEffect(() => {
+      const getData = async () => {
+        const response = await axios.put('/api/hotel/caracteristicas', {
+            description: "Pedro",
+            abreviature: "ped",
+            details: "This is a pedro"
+        })
+        console.log(response.data);
+      };
+      getData();
+    }, []);
+
+
     return (
         <>
-            HomePage
+            {/* <ul>
+            {caracteristics.map((caracteristic, index) => (
+                <li key={index}>{caracteristic.description}</li>
+            ))}
+            </ul> */}
         </>
     )
 }
 
-export default Homepage; 
+export default Homepage;
