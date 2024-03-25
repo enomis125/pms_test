81ff5db5e0eece57fdaeb06935c38d8a0f514e7f
pmonteiro-micronet <pmonteiro.micronet@gmail.com>
2024-03-25 22:15:46 +0000

    Revert "Prisma lib and paths alias"
    This reverts commit c0dde666a5c09ba5eba5bb00aa8e8e1a36071ffd.

--- src/app/api/v1/hotel/caracteristicas/[id]/route.js
+++ src/app/api/v1/hotel/caracteristicas/[id]/route.js
@@ -1,11 +1,23 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
-import prisma from "@/lib/prisma"
+import { PrismaClient } from "@prisma/client";
 
 export async function GET(request, context) {
 
+    const prisma = new PrismaClient()
+
+    // console.log("1")
+
+    // const pathname = new URL(request.url).pathname;
+
+    // const parts = pathname.split('/');
+
+    // const id = parts[parts.length - 1];
+
     const { id } = context.params;
+
+    console.log(id)
 
     const response = await prisma.characteristics.findUnique({
         where: {
@@ -14,21 +26,23 @@ export async function GET(request, context) {
     })
 
     if (!response) {
-        return new NextResponse(JSON.stringify({ status: 404 }));
+        return new NextResponse(JSON.stringify({status: 404 }));
     }
 
     prisma.$disconnect()
 
-    return new NextResponse(JSON.stringify({ response, status: 200 }));
+    return new NextResponse(JSON.stringify({response, status: 200 }));
 }
 
 export async function PATCH(request, context) {
+
+    const prisma = new PrismaClient()
 
     try {
         const { id } = context.params;
         const { data } = await request.json();
 
-        const response = await prisma.characteristics.update({
+        const updateRecord = await prisma.characteristics.update({
             where: {
                 characteristicID: parseInt(id),
             },
@@ -38,7 +52,7 @@ export async function PATCH(request, context) {
                 details: data.details
             }
         })
-        return new NextResponse(JSON.stringify({ status: 200 }));
+        return new NextResponse(JSON.stringify({status: 200 }));
 
     } catch (error) {
         return new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
@@ -50,15 +64,19 @@ export async function PATCH(request, context) {
 
 export async function DELETE(request, context) {
 
+    const prisma = new PrismaClient()
+
     try {
         const { id } = context.params;
 
-        const response = await prisma.characteristics.delete({
+        console.log(id)
+
+        const deleteRecord = await prisma.characteristics.delete({
             where: {
                 characteristicID: parseInt(id),
             }
         })
-        return new NextResponse(JSON.stringify({ status: 200 }));
+        return new NextResponse(JSON.stringify({status: 200 }));
 
     } catch (error) {
         return new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
