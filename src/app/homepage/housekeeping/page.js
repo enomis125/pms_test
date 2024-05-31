8f4582280d7097480074e1d51eaf20d99b92a04a
PHenriques36 <pjvhenriques@gmail.com>
2024-05-31 14:38:52 +0100

    Cards

--- src/app/homepage/housekeeping/page.js
+++ src/app/homepage/housekeeping/page.js
@@ -6,6 +6,7 @@ import Cartao from "@/components/Cards/houseKeeping/Card";
 import { PiListPlus, PiListDashesBold } from "react-icons/pi";
 import { FaBoxesStacked } from "react-icons/fa6";
 import { FaBed, FaWrench, FaPencilRuler } from "react-icons/fa";
+import { FiPlus } from "react-icons/fi";
 
 
 const Card = () => {
@@ -13,16 +14,16 @@ const Card = () => {
         <>
         <div className="border grid grid-cols-4 gap-4 justify-between py-5 px-5">
         <div className="">
-                <Cartao counter1={"29" } title={"LIMPOS"} description={""}  listType={"#"} icon={<PiListPlus size={35} />} formTypeCard={0}/>
+                <Cartao counter1={"29" } title={"LIMPOS"} description={""}  listType={"#"} icon={<PiListPlus size={35} />}  formTypeCard={0}/>
             </div>
             <div className="">
-                <Cartao counter1={"12"} title={"VERIFICADOS"} description={""}  listType={"#"} icon={<PiListPlus size={35} />} formTypeCard={0}/>
+                <Cartao counter2={"12"} title={"VERIFICADOS"} description={""}  listType={"#"} icon={<PiListPlus size={35} />} formTypeCard={0}/>
             </div>
             <div className="">
-                <Cartao counter1={"53"}  title={"SUJOS"} description={""}  listType={"#"} icon={<FaBoxesStacked size={35}/>} formTypeCard={0}/>
+                <Cartao counter3={"53"}  title={"SUJOS"} description={""}  listType={"#"} icon={<FaBoxesStacked size={35}/>} formTypeCard={0}/>
             </div>
             <div className="">
-                <Cartao counter1={"0"} title={"FORA DE SERVIÇO"} description={""}   icon={<PiListDashesBold  size={35}/>} listType={"#"} formTypeCard={0}/>
+                <Cartao counter4={"0"} title={"FORA DE SERVIÇO"} description={""}   icon={<PiListDashesBold  size={35}/>} listType={"#"} formTypeCard={0}/>
             </div>
         </div>
         </>
